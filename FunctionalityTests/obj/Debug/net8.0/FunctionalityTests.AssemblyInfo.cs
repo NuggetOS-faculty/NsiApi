4701aa59878934ed00ee73a8c3292bb77ac16ece
Purgenta <onlinenidza352@gmail.com>
2024-05-12 18:47:15 +0200

    Push Config update

--- FunctionalityTests/obj/Debug/net8.0/FunctionalityTests.AssemblyInfo.cs
+++ FunctionalityTests/obj/Debug/net8.0/FunctionalityTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunctionalityTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbc74e82e8604be3dd46ec721e6f9e9352851c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f79dee4b06a6654275915981912db055046d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunctionalityTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunctionalityTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
