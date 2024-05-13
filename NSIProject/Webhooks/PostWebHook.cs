e5d729211a7de47f4fa1c059c1234da35621c60b
Purgenta <onlinenidza352@gmail.com>
2024-05-13 23:16:00 +0200

    Added webhook

---
+++ NSIProject/Webhooks/PostWebHook.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSIProject.Application.Commands.Post;
+using NSIProject.Application.Common.Dto.Post;
+using NSIProject.Auth.Constants;
+
+namespace NSIProject.Webhooks;
+
+public class PostWebHook : BaseWebHook
+{
+    [HttpPost]
+    [Authorize(AuthenticationSchemes = nameof(AuthConstants.HeaderDbAuthSchema))]
+    public async Task<IActionResult> CreatePost([FromBody] CreatePostDto postDto)
+    {
+        return Ok(await Mediator.Send(new CreatePostCommand(postDto)));
+    }
+}
--- NSIProject/obj/Debug/net8.0/NSIProject.Api.AssemblyInfo.cs
+++ NSIProject/obj/Debug/net8.0/NSIProject.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NSIProject.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a5ec751083bfe86087381ded9ce97437ce333bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80e67473efecd0a8704b4b0e49131fa9a5c55e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NSIProject.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NSIProject.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
